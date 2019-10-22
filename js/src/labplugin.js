3101141612872a6cb5ba5a8a0ef2ea389bc0c961
artur.trzesiok <artur.trzesiok@gmail.com>
2019-10-22 20:22:15 +0200

    html snapshot doesn't render on chrome #142 Doubled labels in Jupyter Lab #131

--- js/src/labplugin.js
+++ js/src/labplugin.js
@@ -12,6 +12,9 @@ module.exports = {
     id: 'jupyter.extensions.k3d',
     requires: [base.IJupyterWidgetRegistry],
     activate: function (app, widgets) {
+        require('style-loader?{attrs:{id: "k3d-katex"}}!css-loader!../node_modules/katex/dist/katex.min.css');
+        require('style-loader?{attrs:{id: "k3d-dat.gui"}}!css-loader!../node_modules/dat.gui/build/dat.gui.css');
+
         widgets.registerWidget({
             name: 'k3d',
             version: version,
