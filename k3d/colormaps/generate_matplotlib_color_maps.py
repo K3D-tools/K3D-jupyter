c1fe658e56adee5d46126e7fcb74380bae014233
artur.trzesiok <artur.trzesiok@gmail.com>
2020-02-12 11:16:20 +0100

    Color maps consistency and convenient access #205

--- k3d/colormaps/generate_matplotlib_color_maps.py
+++ k3d/colormaps/generate_matplotlib_color_maps.py
@@ -4,17 +4,19 @@ from matplotlib import pyplot, cm
 
 min_samples = 256
 with open('matplotlib_color_maps.py', 'w') as file:
+    file.write('import numpy as np\n\n')
+
     for name in sorted(pyplot.colormaps()):
         cmap = cm.get_cmap(name)
         name_c = name.capitalize()
         if name_c == name:
-            file.write('{} = [ \n'.format(name))
+            file.write('{} = np.array([ \n'.format(name))
         else:
             # compability with older matplotlib_color_maps.py where all names were capitalized
-            file.write('{} = {} = [ \n'.format(name, name_c))
+            file.write('{} = {} = np.array([ \n'.format(name, name_c))
 
         # cmap.N is the actual number of datapoints the map is constructed with
         for x in np.linspace(0, 1, max(cmap.N, min_samples)):
             r, g, b = cmap(x)[:3]
             file.write('    {x}, {r}, {g}, {b},\n'.format(**locals()))
-        file.write(']\n\n')
+        file.write('], dtype=np.float32)\n\n')
