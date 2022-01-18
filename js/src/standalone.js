547745066b082226e4b0affdbbb804d18d714461
artur.trzesiok@gmail.com <artur.trzesiok@gmail.com>
2022-01-18 23:57:51 +0100

    updates from k3d-pro

--- js/src/standalone.js
+++ js/src/standalone.js
@@ -1,10 +1,16 @@
 const msgpack = require('msgpack-lite');
-const pako = require('pako');
+const fflate = require('fflate');
 const TFEdit = require('./transferFunctionEditor');
+const serialize = require('./core/lib/helpers/serialize');
 const K3D = require('./core/Core');
 const ThreeJsProvider = require('./providers/threejs/provider');
 
 const MsgpackCodec = msgpack.createCodec({ preset: true });
+
+window.Float16Array = require('./core/lib/helpers/float16Array');
+
+MsgpackCodec.addExtPacker(0x20, Float16Array, (val) => val);
+MsgpackCodec.addExtUnpacker(0x20, (val) => Float16Array(val.buffer));
 
 require('katex/dist/katex.min.css');
 require('dat.gui/build/dat.gui.css');
@@ -14,37 +20,45 @@ function msgpackDecode(data) {
 }
 
 function CreateK3DAndLoadBinarySnapshot(data, targetDOMNode) {
-    let K3DInstance;
+    return new Promise((resolve, reject) => {
+        let K3DInstance;
 
-    data = pako.inflate(data);
-    data = msgpackDecode(data);
-
-    try {
-        K3DInstance = new K3D(
-            ThreeJsProvider,
-            targetDOMNode,
-            data.plot,
-        );
-    } catch (e) {
-        console.log(e);
-        return null;
-    }
-
-    return K3DInstance.setSnapshot(data).then(() => {
-        setTimeout(() => {
-            if (data.plot.camera.length > 0) {
-                K3DInstance.setCamera(data.plot.camera);
-                K3DInstance.render();
+        fflate.unzlib(new Uint8Array(data), (err, decompressData) => {
+            if (!err) {
+                data = decompressData;
             }
-        }, 10);
 
-        return K3DInstance;
+            data = msgpackDecode(data);
+
+            try {
+                K3DInstance = new K3D(
+                    ThreeJsProvider,
+                    targetDOMNode,
+                    data.plot,
+                );
+            } catch (e) {
+                console.log(e);
+                return reject(e);
+            }
+
+            K3DInstance.setSnapshot(data).then(() => {
+                setTimeout(() => {
+                    if (data.plot.camera.length > 0) {
+                        K3DInstance.setCamera(data.plot.camera);
+                        K3DInstance.render();
+                    }
+                }, 10);
+
+                resolve(K3DInstance);
+            });
+        });
     });
 }
 
 module.exports = {
     K3D,
     msgpackDecode,
+    serialize,
     CreateK3DAndLoadBinarySnapshot,
     TransferFunctionEditor: TFEdit.transferFunctionEditor,
     TransferFunctionModel: TFEdit.transferFunctionModel,
