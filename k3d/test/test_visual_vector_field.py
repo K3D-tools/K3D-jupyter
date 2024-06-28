f0622a5fc5416b6cdb5f5651b85f537b4cf34670
Artur Trzesiok <artur.trzesiok@gmail.com>
2024-06-28 13:58:48 +0200

    test references

--- k3d/test/test_visual_vector_field.py
+++ k3d/test/test_visual_vector_field.py
@@ -11,14 +11,15 @@ def f(x, y):
     return np.sin(float(x) / width * np.pi * 2.0), np.cos(float(y) / height * np.pi * 2.0)
 
 
-colors = np.array([[(0xFF0000, 0x00FF00) for x in range(width)] for y in range(height)])
-vectors = np.array([[f(x, y) for x in range(width)] for y in range(height)])
+colors = np.array([[(0xFF0000, 0x00FF00) for x in range(width)] for y in range(height)], dtype=np.uint32)
+vectors = np.array([[f(x, y) for x in range(width)] for y in range(height)], dtype=np.float32)
 
 
 def test_vector_field():
     global vectors, colors, model_matrix
 
     prepare()
+    pytest.plot.grid_visible = False
 
     vector_field = k3d.vector_field(vectors[::5, ::5], colors[::5, ::5], scale=1.5,
                                     line_width=0.001, head_size=1.5,
@@ -33,6 +34,7 @@ def test_vector_field_no_head():
     global vectors, colors, model_matrix
 
     prepare()
+    pytest.plot.grid_visible = False
 
     vector_field = k3d.vector_field(vectors, colors, scale=1.5, use_head=False, line_width=0.001,
                                     transform=k3d.transform(rotation=[np.pi / 2, 1, 0, 0]))
@@ -46,6 +48,7 @@ def test_vector_field_scale():
     global vectors, colors, model_matrix
 
     prepare()
+    pytest.plot.grid_visible = False
 
     vector_field = k3d.vector_field(vectors, colors, scale=2.5, line_width=0.001,
                                     transform=k3d.transform(rotation=[np.pi / 2, 1, 0, 0]))
--- k3d/test/test_visual_vector_field_3d.py
+++ k3d/test/test_visual_vector_field_3d.py
@@ -8,13 +8,14 @@ width = height = length = 10
 
 
 def f(x, y, z):
-    return (np.sin(float(x) / width * np.pi * 2.0) * 1.05,
-            np.cos(float(y) / height * np.pi * 2.0) * 1.05,
-            np.sin(float(z) / length * np.pi * 2.0) * 1.05)
+    return (np.sin(float(x) / width * np.pi * 2.0),
+            np.cos(float(y) / height * np.pi * 2.0),
+            np.sin(float(z) / length * np.pi * 2.0))
 
 
 colors = np.array(
-    [[[(0xFF0000, 0x00FF00) for x in range(width)] for y in range(height)] for z in range(length)])
+    [[[(0xFF0000, 0x00FF00) for x in range(width)] for y in range(height)] for z in range(length)],
+    dtype=np.uint32)
 vectors = np.array(
     [[[f(x, y, z) for x in range(width)] for y in range(height)] for z in range(length)],
     dtype=np.float32)
@@ -24,6 +25,7 @@ def test_vector_field_3d():
     global vectors, colors, model_matrix
 
     prepare()
+    pytest.plot.grid_visible = False
 
     vector_field = k3d.vector_field(vectors[::2, ::2, ::2], colors[::2, ::2, ::2], scale=1.5,
                                     line_width=0.001, head_size=1.5,
@@ -38,6 +40,7 @@ def test_vector_field_3d_no_head():
     global vectors, colors, model_matrix
 
     prepare()
+    pytest.plot.grid_visible = False
 
     vector_field = k3d.vector_field(vectors, colors, scale=1.5, use_head=False, line_width=0.001,
                                     transform=k3d.transform(rotation=[np.pi / 2, 1, 0, 0]))
@@ -51,6 +54,7 @@ def test_vector_field_3d_scale():
     global vectors, colors, model_matrix
 
     prepare()
+    pytest.plot.grid_visible = False
 
     vector_field = k3d.vector_field(vectors, colors, scale=2.5, line_width=0.001,
                                     transform=k3d.transform(rotation=[np.pi / 2, 1, 0, 0]))
